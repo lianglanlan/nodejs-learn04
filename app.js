c56d52f3eb77382668526dc04539be213b7f974a
lianglanlan <799679063@qq.com>
2020-04-08 14:12:53 +0800

    从文件中读取数据

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express')
 const nunjucks = require('nunjucks')
 
@@ -9,9 +10,16 @@ nunjucks.configure('views', { autoescape: true, express: app })
 app.use('/public/', express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        fruits: ['苹果', '香蕉', '橘子', '梨']
+    fs.readFile('./db.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('server error')
+        }
+        res.render('index', {
+            fruits: ['苹果', '香蕉', '橘子', '梨'],
+            students: JSON.parse(data).students
+        })
     })
+
 })
 
 app.listen(3100, () => console.log('running'))
