b39cf5b3f256cf4bebb84caff9fddbaa3fa264d5
lianglanlan <799679063@qq.com>
2020-04-09 10:52:50 +0800

    添加新学生，post获取表单请求主体

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const bodyParser = require('body-parser')
 const router = require('./router')
 const app = express()
 
 app.set('view engine', 'njk')
 nunjucks.configure('views', { autoescape: true, express: app })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/public/', express.static('public'))
 
--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/students', (req, res) => {
     fs.readFile('./db.json', 'utf8', (err, data) => {
         if (err) {
             return res.status(500).send('server error')
@@ -13,6 +13,27 @@ router.get('/', (req, res) => {
             students: JSON.parse(data).students
         })
     })
+})
+
+router.get('/students/new', (req, res) => {
+    res.render('new')
+})
+
+router.post('/students/new', (req, res) => {
+    console.log(req.body)
+    res.send('')
+})
+
+router.get('/students/edit', (req, res) => {
+    res.send('')
+})
+
+router.post('/students/edit', (req, res) => {
+    res.send('')
+})
+
+router.post('/students/delete', (req, res) => {
+    res.send('')
 })
 
 module.exports = router
